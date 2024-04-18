4714cbaaf97771e70ad396e8ddd8b1ad9ac6a69f
DB Christenson <dbeauchristenson@gmail.com>
2024-04-18 01:24:21 +0000

    started styling

--- app.rb
+++ app.rb
@@ -10,20 +10,5 @@ EVENT_DATE = "2024-04-27"
 TICKET_PRICE = 10
 
 get("/") do
-
-end
-
-post("/purchase") do
-  # Calculate total price based on quantity and a fixed ticket price
-  total_price = params[:quantity].to_i * TICKET_PRICE
-  
-  # Create a new purchase record
-  purchase = Purchase.create(
-    email: params[:email],
-    quantity: params[:quantity],
-    total_price: total_price,
-    confirmed: false  # Initially false, set to true after payment confirmation
-  )
-
-  # Redirect to payment process
+  send_file 'views/index.html'
 end
